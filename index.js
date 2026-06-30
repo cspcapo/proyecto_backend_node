7aabddcacf9b81cee0dcd3a040a3d68a997b49b2
unknown <cspcapo@gmail.com>
2026-06-30 15:59:18 -0300

    subida sin env server ok

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "./app.js";
+import express from "express";
+
+import productsRouter from "./src/routers/products.routers.js";
+
+const app = express();
+
+app.use("/api/products", productsRouter);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
