359b10089f58a434e83d0d3d2a5a4950dc826710
unknown <cspcapo@gmail.com>
2026-07-03 12:57:33 -0300

    cambios firebase funcionando para crear

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
-import db from "./firebase.js";
+//import db from "./firebase.bak";
+import db from "./src/config/firebase.js";
 import express from "express";
 import { addDoc, collection, getDocs } from "firebase/firestore";
 
-import productsRouter from "./src/routers/products.routers.js";
 
-const crateProduct = async () => {
+import productsRouter from "./src/routers/products.routers.js";
+const createProduct = async () => {
   const product = {
-    id: "005",
+    id: "006",
     categoria: "camara",
-    marca: "ezvis",
+    marca: "gadnic",
     stock: 10,
   };
-
-  try {
-    const result = await addDoc(collection(db, "products"), product);
-    console.log("Documento agregado con ID:", result.id);
-  } catch (error) {
-    console.error("Error al agregar el producto a Firestore:", error);
-  }
+  const result = await addDoc(collection(db, "products"), product);
+  console.log(result);
 };
-
 const app = express();
-app.use("/api/products", productsRouter);
+
+//app.use("/api/products", productsRouter);
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-await crateProduct();
+createProduct();
