b0846ffae452965bc04653884c459cde39075e0d
cspcapo <cspcapo@gmail.com>
2026-07-05 19:03:28 -0300

    solo falta completar capa auth

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
-//import db from "./firebase.bak";
 import db from "./src/config/firebase.js";
 import express from "express";
 import { addDoc, collection, getDocs } from "firebase/firestore";
-
-
 import productsRouter from "./src/routers/products.routers.js";
+import authRouter from "./src/routers/auth.routers.js";
 import notFound from "./src/middlewares/notFound.middleware.js";
+import errorHandler from "./src/middlewares/error.middleware.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/products", productsRouter);
+app.use("/auth/login", authRouter);
 
-// Middleware para rutas desconocidas (debe ir después de las rutas)
+// Middleware para rutas desconocidas
 app.use(notFound);
+
+// Middleware global de manejo de errores
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
