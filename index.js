0a527d09574826df77ce370e898ae05b1ab056a9
unknown <cspcapo@gmail.com>
2026-07-06 15:32:19 -0300

    todo funcionando y verificado menos readme falta completar y probar con deploid

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/products", productsRouter);
 app.use("/api/auth", authRouter);
+// `express.json()` already handles JSON body parsing.
 
 // Middleware para rutas desconocidas
 app.use(notFound);
